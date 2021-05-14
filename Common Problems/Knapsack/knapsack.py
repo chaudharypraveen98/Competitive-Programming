4a97fb7811d10663f716ba39a73f8fa2fca4179d
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2021-05-14 12:30:03 +0530

    Common Problem Knapsack

---
+++ Common Problems/Knapsack/knapsack.py
@@ -0,0 +1,33 @@
+def knapsack(index, W):
+
+    # if the value is already calculated
+    if array[index][W] != -1:
+        return array[index][W]
+    
+    # When index ie zero or total available space is over
+    if index == 0 or W == 0:
+        result = 0
+
+    # If the value at that index is greater than total available space
+    elif weight[index] > W:
+        result = knapsack(index - 1, W)
+    
+    # we will calculate the max value by including or excluding this element.
+    else:
+        temp1 = knapsack(index - 1, W)
+        temp2 = values[index] + knapsack(index - 1, W - weight[index])
+        result = max(temp1, temp2)
+    array[index][W] = result
+    return result
+
+
+if __name__ == '__main__':
+    weight = [10, 20, 30, 40, 50]
+    values = [60, 100, 120, 135, 200]
+    max_weight = 100
+    length = len(weight)
+
+    # intialising the array
+    array = [[-1 for col in range(max_weight + 1)] for row in range(length + 1)]
+    final_result = knapsack(length - 1, max_weight)
+    print(final_result)
---
+++ Common Problems/Knapsack/readme.md
@@ -0,0 +1,4 @@
+## Knapsack Problem
+
+The knapsack problem is a problem in combinatorial optimization: Given a set of items, each with a weight and a value, determine the number of each item to include in a collection so that the total weight is less than or equal to a given limit and the total value is as large as possible.
+
