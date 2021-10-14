4d39f1b796cde72d4e20e1d9a61445d5e53ef907
Praveen Chaudhary <chaudharypraveen98@gmail.com>
2021-10-14 21:49:03 +0530

    FROM HACKERRANK Concatenate

---
+++ Python/Hackerrank/Concatenate/concatenate.py
@@ -0,0 +1,5 @@
+import numpy as np
+a, b, c = map(int,input().split())
+arrA = np.array([input().split() for _ in range(a)],int)
+arrB = np.array([input().split() for _ in range(b)],int)
+print(np.concatenate((arrA, arrB), axis = 0))
