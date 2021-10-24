431e83e96c65d11c1084ee15862cb14962e4fde1
Praveen Chaudhary <chaudharypraveen98@gmail.com>
2021-10-24 20:46:15 +0530

    FROM HACKERRANK Lists

---
+++ Python/Hackerrank/Lists/lists.py
@@ -0,0 +1,11 @@
+N = int(input())
+l = []
+for _ in range(N):
+    s = input().split()
+    cmd = s[0]
+    args = s[1:]
+    if cmd != "print":
+        cmd += "(" + ",".join(args) + ")"
+        eval("l." + cmd)
+    else:
+        print(l)
