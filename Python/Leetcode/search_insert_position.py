c00465a11f12ada03ede6ff776cfca666cfe53dd
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2022-07-31 21:41:19 +0530

    search_insert_position

---
+++ Python/Leetcode/search_insert_position.py
@@ -0,0 +1,13 @@
+# Its a binary search approach
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)
+        while low < high:
+            mid = (low + high) // 2
+            if target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+        
