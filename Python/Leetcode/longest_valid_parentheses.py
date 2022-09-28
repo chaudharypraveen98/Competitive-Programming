16e0d6988f35aa3c5c1f3e6c09adee2644497352
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2022-09-29 01:04:25 +0530

    trying approach

---
+++ Python/Leetcode/longest_valid_parentheses.py
@@ -0,0 +1,11 @@
+def longest_valid_parentheses(num):
+  for i in range(len(num)):
+    pass
+
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+# (_ _ _ _) => ((_ _ _)
+
+if __name__ == "__main__":
+  nums = 2
+  print(longest_valid_parentheses(nums))
