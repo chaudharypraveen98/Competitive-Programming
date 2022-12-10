5b03f6a947998aa3bdf8bd8cecdea47b2684f16f
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2022-12-10 23:20:29 +0530

    LEETCODE Rotate Image

--- Python/Leetcode/Rotate Image/rotate-image.md
+++ Python/Leetcode/Rotate Image/rotate-image.md
@@ -28,3 +28,13 @@ we can start filling the table from column 4.
 |---|---|---|---|:-:|---|
 | 4 | 5 | 6 | 5 | . | . |
 | 7 | 8 | 9 | . | . | . |
+
+
+Approach 2: Reverse on Diagonal and then Reverse Left to Right
+https://leetcode.com/problems/rotate-image/solutions/1037232/rotate-image/
+
+Approach 3 : Rotate Groups of Four Cells
+
+1. We are going to rotate four element in one go.
+2. Store the topmost element in a temp variable and rotate in reverse order
+<img src="./rotation_pattern.png">
--- Python/Leetcode/Rotate Image/rotate-image.py
+++ Python/Leetcode/Rotate Image/rotate-image.py
@@ -8,6 +8,46 @@ class Solution:
             for j in range(n):
                 matrix[j].append(matrix[i][j])
             matrix[i]=matrix[i][n:]
+    
+    def rotateByFourDiagonal(self, matrix):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
 
+        # you can use len(matrix[0]). we have used len(matrix) because its a nxn matrix
+        left,right = 0, len(matrix)-1
+
+        while left<right:
+            # loop entire row except last element (left,right-1)
+            for i in range(right-left):
+                top,bottom = left,right
+
+                # storing the top left value
+                top_left = matrix[top][left+i]
+
+                # swapping bottom left with top left
+                matrix[top][left+i] = matrix[bottom-i][left]
+
+                # swapping bottom right with bottom left
+                matrix[bottom-i][left] =matrix[bottom][right-i]
+
+                #swapping bottom right with top right
+                matrix[bottom][right-i] = matrix[top+i][right]
+
+                matrix[top+i][right]=top_left
+            left+=1
+            right-=1
+        
+    def rotateByTransposeAndReverse(self, matrix):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        for row in range(1, len(matrix)):
+            for col in range(row):
+                matrix[row][col],matrix[col][row]= matrix[col][row],matrix[row][col]
+        for row in range(len(matrix)):
+            matrix[row]=matrix[row][::-1]
 item  = Solution()
-print(item.rotate([[1,2,3],[4,5,6],[7,8,9]]))
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+item.rotateByTransposeAndReverse(matrix)
+print(matrix)
