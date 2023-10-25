d1c6ecc3e2c72e73c26d9eeb9751982583f7c575
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2023-10-25 23:52:21 +0530

    LEETCODE 15. 3Sum

---
+++ Leetcode/15. 3Sum/readme.md
@@ -0,0 +1,6 @@
+## Approach
+1. First we will sort the array which will allow us to use two way pointer depending on the sum of the value
+2. We will be loop i from 0...n
+3. Then use the two way pointer left and right
+4. To avoid duplicates, we will try that i must not repeat and and j and k too
+5. Hurray we have got the answers
---
+++ Leetcode/15. 3Sum/three_sum.rs
@@ -0,0 +1,39 @@
+struct Solution {}
+
+impl Solution {
+    pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut res: Vec<Vec<i32>> = Vec::new();
+        let mut nums = nums;
+        nums.sort();
+        for i in 0..nums.len() - 2 {
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue;
+            }
+            let mut left = i;
+            let mut right = nums.len() - 1;
+            while left < right {
+                let sum_three = nums[i] + nums[left] + nums[right];
+                if sum_three == 0 {
+                    res.push(vec![nums[i], nums[left], nums[right]]);
+                    while left < right && nums[left] == nums[left + 1] {
+                        left += 1;
+                    }
+                    while left < right && nums[right] == nums[right - 1] {
+                        right -= 1;
+                    }
+                    left += 1;
+                    right -= 1;
+                } else if sum_three > 0 {
+                    right -= 1;
+                } else {
+                    left += 1;
+                }
+            }
+        }
+        res
+    }
+}
+fn main() {
+    let res = Solution::three_sum(vec![-1, 0, 1, 2, -1, -4]);
+    println!("{:?}", res);
+}
