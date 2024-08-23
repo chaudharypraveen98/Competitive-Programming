a21fad395c82f45430cf812000b175ea0c3f2032
profumato4 <Leonardobelli2006@gmail.com>
2024-08-23 17:12:13 +0200

    Java - Java BigInteger

---
+++ Java/Hackerrank/Java BigInteger/Solution.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BigInteger a = new BigInteger(scan.next());
+        BigInteger b = new BigInteger(scan.next());
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
