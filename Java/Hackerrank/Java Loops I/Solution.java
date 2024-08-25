6c702417f9a38cbb92002318db8ae51809d837b1
profumato4 <Leonardobelli2006@gmail.com>
2024-08-25 22:47:40 +0200

    Java - Java Loops I

---
+++ Java/Hackerrank/Java Loops I/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+
+        for(int i = 1; i <= 10;i++){
+
+            System.out.println(n + " x " + i + " = " + n*i);
+
+
+        }
+
+    }
+}
