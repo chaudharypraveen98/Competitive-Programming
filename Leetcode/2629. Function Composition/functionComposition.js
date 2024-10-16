62c9c696c580a87f7b4734e725f515c2b45494e0
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2024-10-16 23:12:23 +0530

    LEETCODE - 2629. Function Composition

---
+++ Leetcode/2629. Function Composition/functionComposition.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+  let res;
+  return function (x) {
+    res = x;
+    for (let i = functions.length - 1; i >= 0; i--) {
+      res = functions[i](res);
+    }
+    return res;
+  };
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
---
+++ Leetcode/2629. Function Composition/readme.md
@@ -0,0 +1,6 @@
+Link -> [2629. Function Composition](https://leetcode.com/problems/function-composition/description/?envType=study-plan-v2&envId=30-days-of-javascript)
+
+## Approach
+1. Simply init the res with init value
+2. loop through the functions using for loop and apply from `right` to `left`
+3. Hurray! Done
