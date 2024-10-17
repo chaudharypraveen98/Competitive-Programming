3459d6add3b585962e6bbae4fa2c1b8af12377e8
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2024-10-17 17:32:24 +0530

    LEETCODE  - 2677. Chunk Array

---
+++ Leetcode/2677. Chunk Array/chunkArray.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+const chunk = (arr, size) => {
+  return arr.reduce((chunkedArray, element) => {
+    const lastChunk = chunkedArray[chunkedArray.length - 1];
+    if (!lastChunk || lastChunk.length === size) {
+      chunkedArray.push([element]);
+    } else {
+      lastChunk.push(element);
+    }
+    return chunkedArray;
+  }, []);
+};
---
+++ Leetcode/2677. Chunk Array/readme.md
@@ -0,0 +1,7 @@
+Link -> [2677. Chunk Array](https://leetcode.com/problems/chunk-array/?envType=study-plan-v2&envId=30-days-of-javascript)
+
+## Approach
+1. You can use the for loop or reduce
+2. for reduce, lastChunk contains the latest chunk
+3. chunkArray contains final array
+4. Hurray ! Done
