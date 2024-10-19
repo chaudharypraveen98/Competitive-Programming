539c17f493c28d6816df60ef7aad90559f0a45d1
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2024-10-19 22:26:18 +0530

    NEETCODE - ALTERNATE WAY

---
+++ Neetcode/Longest Palindromic Substring/2_longest_palindromic_substring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        largest = ""
+
+        def expand_from_center(i, j):
+            while i >= 0 and j < len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+            return s[i+1:j]
+        for i in range(len(s)-1):
+            odd = expand_from_center(i, i)
+            even = expand_from_center(i, i+1)
+            if len(odd) > len(largest):
+                largest = odd
+            if len(even) > len(largest):
+                largest = even
+        return largest
