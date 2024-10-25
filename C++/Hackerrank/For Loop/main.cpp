3de0560a3d4de4346f45683919003c89a67c782f
Ruslan Sedov <rse@neiry-bci.com>
2024-10-25 22:08:56 +0300

    For Loop - SOLVED HACKERRANK PROBLEM

---
+++ C++/Hackerrank/For Loop/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+void printNumberDescription(int a, int b) {
+    // Array of strings for English names of numbers from 1 to 9
+    const std::string numberWords[] = {
+        "zero", "one", "two", "three", "four", 
+        "five", "six", "seven", "eight", "nine"
+    };
+
+    // We go through all the numbers from a to b inclusive
+    for (int n = a; n <= b; ++n) {
+        if (n >= 1 && n <= 9) {
+            std::cout << numberWords[n] << std::endl;
+        } else if (n > 9 && n % 2 == 0) {
+            std::cout << "even" << std::endl;
+        } else {
+            std::cout << "odd" << std::endl; 
+        }
+    }
+}
+
+int main() {
+    int a;
+    int b;
+    std::cin >> a >> b;
+    printNumberDescription(a, b);
+}
