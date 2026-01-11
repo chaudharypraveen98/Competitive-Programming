02727148ef2009e7be3d7b6e79340b50f0994043
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-01-11 14:31:40 +0530

    LEETCODE : 219. Contains Duplicate II

---
+++ Leetcode/219. Contains Duplicate II/219. containsDuplicate_ II.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i]) && i - map.get(nums[i]) <= k) {
+            return true;
+        }
+        map.set(nums[i], i);
+    }
+    return false;
+};
+console.log(containsNearbyDuplicate([1,2,3,1], 3))
---
+++ Leetcode/219. Contains Duplicate II/readme.md
@@ -0,0 +1,4 @@
+## Approach
+1. Understand as "Find 2 numbers in the array that are equal and are at most k apart from each other."
+2. Use a hashmap
+3. Hurray !!
