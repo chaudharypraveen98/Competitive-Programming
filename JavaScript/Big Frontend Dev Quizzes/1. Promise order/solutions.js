1fec62e3f6918e0407d5e5fec2611545e54c7a28
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-04-26 20:43:59 +0530

    BIGFRONTEND QUIZZES: 1. Promise order

---
+++ JavaScript/Big Frontend Dev Quizzes/1. Promise order/solutions.js
@@ -0,0 +1,37 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log(1)
+const promise = new Promise((resolve) => {
+  // sync task inside declaration will print
+  console.log(2)
+  resolve()
+  console.log(3)
+})
+
+console.log(4)
+
+promise.then(() => {
+  console.log(5)
+}).then(() => {
+  console.log(6)
+})
+
+console.log(7)
+
+setTimeout(() => {
+  console.log(8)
+}, 10)
+
+setTimeout(() => {
+  console.log(9)
+}, 0)
+
+// 1
+// 2
+// 3
+// 4
+// 7
+// 5
+// 6
+// 9
+// 8
