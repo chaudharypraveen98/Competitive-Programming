641791bc309d83b31d8754338c46a25648326de9
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-06 23:25:17 +0530

    BIGFRONTEND QUIZZES: 26. true or false

---
+++ JavaScript/Big Frontend Dev Quizzes/26. true or false/readme.md
@@ -0,0 +1,5 @@
+## [26. true or false](https://bigfrontend.dev/quiz/true-or-false)
+
+### Approach
+1. Boolean(new Boolean(false)) -->	true --> Wrapper objects are truthy, even if their inner value is false.
+2. Logical coersion vs implicit coersion
---
+++ JavaScript/Big Frontend Dev Quizzes/26. true or false/solutions.js
@@ -0,0 +1,19 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log([] == 0)
+console.log([] == false)
+console.log(!![]) // logical coersion
+console.log([1] == 1)
+console.log(!![1])
+console.log(Boolean([]))
+console.log(Boolean(new Boolean([])))
+console.log(Boolean(new Boolean(false)))
+
+// true
+// true
+// true
+// true
+// true
+// true
+// true
+// true
