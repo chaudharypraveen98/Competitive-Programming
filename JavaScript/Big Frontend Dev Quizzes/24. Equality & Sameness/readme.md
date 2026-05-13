cd99a6ddee997cea52dd6916278995981fe366bc
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-13 23:09:53 +0530

    BIGFRONTEND QUIZZES: 24. Equality & Sameness

---
+++ JavaScript/Big Frontend Dev Quizzes/24. Equality & Sameness/readme.md
@@ -0,0 +1,6 @@
+## [24. Equality & Sameness](https://bigfrontend.dev/quiz/Equality-Sameness)
+
+1. [Implicit Coercion](../8.%20Implicit%20Coercion%20I//readme.md)
+2. [Implicit Coercion II](../11.%20Implicit%20Coercion%20II/readme.md)
+3. [Equal](../10.%20Equal/readme.md)
+4. Golden rule : The Object.is() method is more precise. It can distinguish between $0$ and $-0$, and it correctly identifies that NaN is equal to itself.
---
+++ JavaScript/Big Frontend Dev Quizzes/24. Equality & Sameness/solutions.js
@@ -0,0 +1,37 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log(0 == '0')
+console.log(0 === '0')
+console.log(Object.is(0, '0'))
+
+console.log(0 == 0)
+console.log(0 === 0)
+console.log(Object.is(0, 0))
+
+console.log(0 == -0)
+console.log(0 === -0)
+console.log(Object.is(0, -0))
+
+console.log(NaN == NaN)
+console.log(NaN === NaN)
+console.log(Object.is(NaN, NaN))
+
+console.log(0 == false)
+console.log(0 === false)
+console.log(Object.is(0, false))
+
+// true
+// false
+// false
+// true
+// true
+// true
+// true
+// true
+// false
+// false
+// false
+// true
+// true
+// false
+// false
