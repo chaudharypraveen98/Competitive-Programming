37bb743e90159c91ad6fdb170513731426995cb2
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-14 23:22:32 +0530

    BIGFRONTEND QUIZZES: 30. Equal II

---
+++ JavaScript/Big Frontend Dev Quizzes/30. Equal II/readme.md
@@ -0,0 +1,4 @@
+## [30. Equal II](https://bigfrontend.dev/quiz/Equal-II)
+
+### Approach
+1. Object == Object , implicit coercion doesn't work
---
+++ JavaScript/Big Frontend Dev Quizzes/30. Equal II/solutions.js
@@ -0,0 +1,28 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log([1] == 1)
+console.log([1] == '1')
+console.log(['1'] == '1')
+console.log(['1'] == 1)
+console.log([1] == ['1'])
+console.log(new Boolean(true) == 1)
+console.log(new Boolean(true) == new Boolean(true))
+console.log(Boolean(true) == '1')
+console.log(Boolean(false) == [0])
+console.log(new Boolean(true) == '1')
+console.log(new Boolean(false) == [0])
+console.log(null == undefined)
+
+
+// true
+// true
+// true
+// true
+// false
+// true
+// false
+// true
+// true
+// true
+// false
+// true
