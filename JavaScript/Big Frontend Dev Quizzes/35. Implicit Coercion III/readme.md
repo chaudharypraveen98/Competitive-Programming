e39a4be07a46c15e3c085d5bc23d6e88e94a21a9
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-15 21:29:16 +0530

    BIGFRONTEND QUIZZES: 35. Implicit Coercion III

---
+++ JavaScript/Big Frontend Dev Quizzes/35. Implicit Coercion III/readme.md
@@ -0,0 +1,3 @@
+## [35. Implicit Coercion III](https://bigfrontend.dev/quiz/Implicit-Conversion-III)
+
+1. The Unary + Operator: A + placed directly before a value forces it into a number immediately.
---
+++ JavaScript/Big Frontend Dev Quizzes/35. Implicit Coercion III/solutions.js
@@ -0,0 +1,24 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log( [] + {} )
+console.log( + {} )
+console.log( + [] )
+console.log( {} + [])
+console.log( ({}) + [])
+console.log( ({}) + [])
+console.log( ({}) + [])
+console.log( {} +  + [])
+console.log( {} +  + [] + {} )
+console.log( {} +  + [] + {}  + [])
+
+
+// "[object Object]"
+// NaN
+// 0
+// "[object Object]"
+// "[object Object]"
+// "[object Object]"
+// "[object Object]"
+// "[object Object]0"
+// "[object Object]0[object Object]"
+// "[object Object]0[object Object]"
