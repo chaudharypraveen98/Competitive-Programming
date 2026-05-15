70aa2fc682de47014178fc17c38bdd8dfce4dec9
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-15 22:46:25 +0530

    BIGFRONTEND QUIZZES: 38. Hoisting IV

---
+++ JavaScript/Big Frontend Dev Quizzes/38. Hoisting IV/readme.md
@@ -0,0 +1,4 @@
+## [38. Hoisting IV](https://bigfrontend.dev/quiz/Hoisting-IV)
+
+1. function create a local context
+2. let and const are also hoisted but they are temporal dead zone
---
+++ JavaScript/Big Frontend Dev Quizzes/38. Hoisting IV/solutions.js
@@ -0,0 +1,18 @@
+// This is a JavaScript Quiz from BFE.dev
+
+let foo = 10
+function func1() {
+    console.log(foo)
+    var foo = 1
+}
+func1 ()
+
+
+function func2() {
+    console.log(foo)
+    let foo = 1
+}
+func2 ()
+
+// undefined
+// Error
