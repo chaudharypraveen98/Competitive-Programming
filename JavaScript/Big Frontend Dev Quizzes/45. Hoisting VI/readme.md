e18899bc00a1d42a77819ad015fcd35046fa152b
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-16 16:16:25 +0530

    BIGFRONTEND QUIZZES: 45. Hoisting VI

---
+++ JavaScript/Big Frontend Dev Quizzes/45. Hoisting VI/readme.md
@@ -0,0 +1,5 @@
+## [45. Hoisting VI](https://bigfrontend.dev/quiz/Hoisting-VI)
+
+### Approach
+1. Local scope greater priority 
+2. inner can't change global if they have same local variable
---
+++ JavaScript/Big Frontend Dev Quizzes/45. Hoisting VI/solutions.js
@@ -0,0 +1,19 @@
+// This is a JavaScript Quiz from BFE.dev
+
+var foo = 1;
+(function () {
+  console.log(foo);
+  foo = 2;
+  console.log(window.foo);
+  console.log(foo);
+  var foo = 3;
+  console.log(foo);
+  console.log(window.foo)
+})()
+
+
+// undefined
+// 1
+// 2
+// 3
+// 1
