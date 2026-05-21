aa5d96fcb9767b54d75df438390c54a2795bdc57
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-21 22:41:52 +0530

    BIGFRONTEND QUIZZES: 90. array keys

--- JavaScript/Big Frontend Dev Quizzes/90. array keys/solutions.js
+++ JavaScript/Big Frontend Dev Quizzes/90. array keys/solutions.js
@@ -5,3 +5,7 @@ console.log(Reflect.ownKeys([,]))
 console.log(Reflect.ownKeys([1,,2]))
 console.log(Reflect.ownKeys([...[1,,2]]))
 
+// ["length"]
+// ["length"]
+// ["0","2","length"]
+// ["0","1","2","length"]
