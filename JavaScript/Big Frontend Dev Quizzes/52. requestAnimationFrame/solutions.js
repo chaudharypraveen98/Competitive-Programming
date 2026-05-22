6782ce3b4c903151e703d91109fad377ea8c13ab
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-22 22:53:33 +0530

    BIGFRONTEND QUIZZES: 52. requestAnimationFrame

---
+++ JavaScript/Big Frontend Dev Quizzes/52. requestAnimationFrame/solutions.js
@@ -0,0 +1,31 @@
+// This is a JavaScript Quiz from BFE.dev
+
+console.log(1)
+
+setTimeout(() => {
+  console.log(2)
+}, 100)
+
+requestAnimationFrame(() => {
+  console.log(3)
+})
+
+requestAnimationFrame(() => {
+  console.log(4)
+  setTimeout(() => {
+    console.log(5)
+  }, 10)
+})
+
+const end = Date.now() + 200;
+while (Date.now() < end) {
+}
+
+console.log(6)
+
+// 1
+// 6
+// 3
+// 4
+// 2
+// 5
