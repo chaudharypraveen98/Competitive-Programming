1171f005951fc49b75f48c023e5fa31c558ee684
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-05-23 14:09:18 +0530

    BIGFRONTEND QUIZZES: 109. isNaN()

---
+++ JavaScript/Big Frontend Dev Quizzes/109. isNaN()/solutions.js
@@ -0,0 +1,13 @@
+const a = 'BFE.dev'
+const b = 1
+
+console.log(Number.isNaN(a))
+console.log(Number.isNaN(b))
+console.log(isNaN(a))
+console.log(isNaN(b))
+
+
+// false
+// false
+// true
+// false
