a5cece67ac23bc96ae1e759e93e10f26cf0e24dc
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-06-10 00:15:24 +0530

    LEETCODE : 28. Find the Index of the First Occurrence in a String

---
+++ Leetcode/28. Find the Index of the First Occurrence in a String/find_the_index_of_the_first_occurrence_in_a_string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        lps = [0]*len(needle)
+        i, prevLPS = 1,0
+
+        while i <len(needle):
+            if  needle[prevLPS]== needle[i]:
+                lps[i] = prevLPS+1
+                i+=1
+                prevLPS += 1
+            elif prevLPS==0:
+                lps[i] = 0
+                i +=1
+            else:
+                prevLPS = lps[prevLPS-1]
+
+            i, j = 0,0
+            while i< len(haystack):
+                if(haystack[i]==needle[j]):
+                    i += 1
+                    j += 1
+                elif j==0:
+                    i+=1
+                else:
+                    j = lps[j-1]
+                
+                if j>= len(needle):
+                    return i-len(needle)
+            return -1
+        
+sol = Solution()
+print(sol.strStr("sadbutsad","sad"))
---
+++ Leetcode/28. Find the Index of the First Occurrence in a String/readme.md
@@ -0,0 +1,5 @@
+## [28. Find the Index of the First Occurrence in a String](https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/)
+
+
+### Approach 1 - Use Knuth–Morris–Pratt KMP
+Follow [Neetcode](https://www.youtube.com/watch?v=JoF0Z7nVSrA&t=620s)
