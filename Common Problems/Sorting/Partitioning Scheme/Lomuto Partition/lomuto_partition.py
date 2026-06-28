e63f7e958e39f6eb08f4cbb00816aa5f1a9e8bd8
chaudharypraveen98 <chaudharypraveen98@gmail.com>
2026-06-28 17:26:05 +0530

    Common Problems : Lomuto partitioning

--- Common Problems/Sorting/Partitioning Scheme/Lomuto Partition/lomuto_partition.py
+++ Common Problems/Sorting/Partitioning Scheme/Lomuto Partition/lomuto_partition.py
@@ -1,11 +1,23 @@
 def lomuto_partition(arr, low, high):
     i = low - 1
-    pivot = arr[high-1]
-    for j in range(low, high-1):
-        if(arr[j]<pivot):
-            i+=1
+    pivot = arr[high]  # Choosing the last element as pivot
+    
+    for j in range(low, high):
+        if arr[j] < pivot:  # Elements smaller than pivot go to the left
+            i += 1
             arr[i], arr[j] = arr[j], arr[i]
-    arr[i+1], arr[high-1] = arr[high-1], arr[i+1]
-    return arr 
+            
+    # Place the pivot in its correct sorted position
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    
+    # Standard Lomuto returns the index of the pivot
+    return i + 1 
 
-print(lomuto_partition([4,2,8,1,3,7,5,6], 0, 8))
+arr_to_sort = [4, 2, 8, 1, 3, 7, 5, 6]
+print("Original:", arr_to_sort)
+
+# Capture the pivot index
+pivot_index = lomuto_partition(arr_to_sort, 0, 7)
+
+print("Partitioned Array:", arr_to_sort)
+print("Pivot element (6) is now at index:", pivot_index)
